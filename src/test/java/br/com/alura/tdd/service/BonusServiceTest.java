08fe975a858f3b650fae3124391f504e9852a566
Caitano <josiasmartinscaitano@gmail.com>
2023-06-09 12:53:00 -0300

    test: lidando com exceptions

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -9,16 +9,26 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.tdd.modelo.Funcionario;
-import junit.framework.Assert;
 
 class BonusServiceTest {
 
 	@Test
 	void bonusDeveriaSerZeroParaComSalarioMuitoAlto() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
 		
-		Assertions.assertEquals(new BigDecimal("0.00"), bonus);
+		// testar exception no jUnit
+		Assertions.assertThrows(
+				IllegalArgumentException.class, 
+				() -> service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")))
+		);
+		
+		// outra forma de fazer e pegar a mensagem da Exception
+//		try {
+//			service.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000")));
+//			fail("Não deu a exception");
+//		} catch (Exception e) {
+//			Assertions.assertEquals("Funcionario com salário maior que R$10000 não pode receber bonus", e.getMessage());
+//		}
 	}
 	
 	@Test
